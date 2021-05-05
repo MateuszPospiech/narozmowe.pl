84def2c833ed30aeaa68d7d6b965d7d00486e4e0
MateuszPospiech <pospiechmateusz@gmail.com>
2021-05-05 21:43:50 +0200

    Initialisation commit with custom settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>NaRozmowe.pl</h1>
       </header>
+      <main>
+        <br/>
+        Ustawienia początkowe z plikami normalize i resetem.
+      </main>
+      <footer>
+        <br/>
+        Dzikie Kuny &copy; 2021
+      </footer>
     </div>
   );
 }
